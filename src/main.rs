78ddec989f0e3265dd105ae9473b34c405f82522
Gu Junhua <jhgu@nao.cas.cn>
2018-02-28 22:39:18 +0800

    新文件：   .idea/misc.xml 	新文件：   .idea/vcs.xml 	新文件：   Cargo.lock 	新文件：   Cargo.toml 	新文件：   src/main.rs

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+extern crate fitsio;
+use fitsio::fitsfile::ImageDescription;
+use std::fs::remove_file;
+
+
+
+fn main() {
+
+    let mut shape = vec![512,1024];//I want to generate a 512-row, 1024-col file
+    shape.reverse();//if this line removed, the generated fits file will be 1024-row 512-col
+    let img_desc = ImageDescription {
+        data_type: fitsio::types::ImageType::LONG_IMG,
+        dimensions: shape.as_slice(),
+    };
+
+    let fname="a.fits".to_string();
+
+    let mut fits_file = {
+        remove_file(&fname);
+        match fitsio::FitsFile::create(fname)
+            .with_custom_primary(&img_desc)
+            .open()
+            {
+                Ok(x) => x,
+                Err(x) => {
+                    println!("{}", x);
+                    panic!("error");
+                }
+            }
+    };
+    //let hdu = fits_file.create_image("".to_string(), &img_desc)?;
+    let hdu = fits_file.current_hdu().unwrap();
+    let mut data1 = Vec::<i32>::new();
+    data1.resize(512*1024, 0);
+    hdu.write_section(&mut fits_file, 0, data1.len(), &data1);
+}
